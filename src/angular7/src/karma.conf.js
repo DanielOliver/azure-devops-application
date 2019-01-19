20b084c94a983351f134b37e58b1c32f3cbbd236
Daniel Oliver <danieloliversoftware@gmail.com>
2019-01-19 17:10:05 -0500

    Add code coverage.

--- src/angular7/src/karma.conf.js
+++ src/angular7/src/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
