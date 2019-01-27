0fde9be21e889c5f8073528f2c119e49e8ab5aa9
Daniel Oliver <danieloliversoftware@gmail.com>
2019-01-27 13:51:46 -0500

    Allow e2e tests to run headless

--- src/angular7/e2e/protractor.conf.js
+++ src/angular7/e2e/protractor.conf.js
@@ -4,13 +4,20 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 const { JUnitXmlReporter } = require('jasmine-reporters');
 
+process.env.CHROME_BIN = process.env.CHROME_BIN || require("puppeteer").executablePath();
+
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu", "--window-size=1200,900"],
+      binary: process.env.CHROME_BIN
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -26,8 +33,8 @@ exports.config = {
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
     var junitReporter = new JUnitXmlReporter({
-      savePath: require('path').join( __dirname, './junit' ),
-      consolidateAll: false
+      savePath: require('path').join(__dirname, './junit'),
+      consolidateAll: true
     });
     jasmine.getEnv().addReporter(junitReporter);
   }
